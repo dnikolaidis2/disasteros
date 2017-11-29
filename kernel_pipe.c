d6c88e67480882356d93850c597b06f9095840a7
Unknown <dnikolaidis4@gmail.com>
2017-11-29 17:33:10 +0200

    Pipe implementation improvment
    -Rewrote read function for simplicity
    -Reworked part of write function to fix errors
    -Basic implementations of reader_close writer_close

--- kernel_pipe.c
+++ kernel_pipe.c
@@ -3,6 +3,7 @@
 #include "kernel_streams.h"
 #include "kernel_dev.h"
 #include "kernel_cc.h"
+
 //@TODO: Should these always be 64 bit?
 #define Kilobytes(Value) ((Value)*1024)
 #define Megabytes(Value) (Kilobytes(Value)*1024)
@@ -16,56 +17,58 @@
 
 typedef struct pipe_control_block {
 	int8_t buffer[BUFF_SIZE];
-	uint32_t writer_p;
-	uint32_t reader_p;
+	uint32_t write_p;
+	uint32_t read_p;
 
 	CondVar hasSpace;
 	CondVar hasData;
 
 	pipe_t* pipe;
 
+	uint32_t reader_closed;
+	uint32_t writer_closed;
+
 } PipeCB;
 
 int pipe_read (void* this, char *buf, unsigned int size)
 {
 	PipeCB* pipe = (PipeCB*) this;
+	uint32_t write_p = pipe->write_p;
+	uint32_t read_p = pipe->read_p;
+	
 	unsigned int bytes_read = 0;
 
-	if (pipe->reader_p == pipe->writer_p)
+	if (pipe->writer_closed)
+	{
+		return 0;
+	}
+
+	if (read_p == write_p)
 	{
 		kernel_wait(&pipe->hasData, SCHED_PIPE);
+		write_p = pipe->write_p;
+		read_p = pipe->read_p;
 	}
-	
-	if ((pipe->reader_p + size) % BUFF_SIZE <= pipe->writer_p)
+
+	int available_data = (BUFF_SIZE - read_p + write_p) % BUFF_SIZE;
+
+	for (int i = 0; i < available_data; ++i)
 	{
-		if (pipe->writer_p < pipe->reader_p && pipe->reader_p + size > BUFF_SIZE)
+		buf[i] = pipe->buffer[read_p];
+		bytes_read++;
+
+		// Increments write pointer by 1, cycling through the array;
+		read_p = (read_p + 1) %(BUFF_SIZE);
+
+		if (bytes_read == size)
 		{
-			memcpy(buf, &pipe->buffer[pipe->reader_p], BUFF_SIZE - pipe->reader_p);
-			memcpy(buf + (BUFF_SIZE - pipe->reader_p), pipe->buffer, (pipe->reader_p + size) % BUFF_SIZE);
-		}
-		else
-		{
-			memcpy(buf, &pipe->buffer[pipe->reader_p], size);
-		}
-		bytes_read += size;
-	}
-	else
-	{
-		bytes_read = size - (pipe->writer_p - (pipe->reader_p + size) % BUFF_SIZE);
-		if (pipe->writer_p < pipe->reader_p && pipe->reader_p + bytes_read > BUFF_SIZE)
-		{
-			memcpy(buf, &pipe->buffer[pipe->reader_p], BUFF_SIZE - pipe->reader_p);
-			memcpy(buf + (BUFF_SIZE - pipe->reader_p), pipe->buffer, (pipe->reader_p + bytes_read) % BUFF_SIZE);
-		}
-		else
-		{
-			memcpy(buf, &pipe->buffer[pipe->reader_p], bytes_read);
+			break;
 		}
 	}
 
 	if (bytes_read)
 	{
-		pipe->reader_p = (pipe->reader_p + bytes_read) % BUFF_SIZE;
+		pipe->read_p = read_p;
 		Cond_Broadcast(&pipe->hasSpace);
 		return bytes_read;
 	}
@@ -77,69 +80,79 @@ int pipe_read (void* this, char *buf, unsigned int size)
 	
 int reader_close (void* this)
 {
+	PipeCB* pipe = (PipeCB *) this;
+	pipe->reader_closed = 1;
   return -1;
 }
 
 int pipe_write (void* this, const char* buf, unsigned int size)
 {	
 	PipeCB* pipe = (PipeCB*) this;
-	uint32_t write_p = pipe->writer_p;
-	uint32_t read_p = pipe->reader_p;
-	uint32_t buff_size = BUFF_SIZE;
+	uint32_t write_p = pipe->write_p;
+	uint32_t read_p = pipe->read_p;
+	int bytes_writen = 0;
+
+	if (pipe->reader_closed)
+	{
+		return -1;
+	}
 
 	// We assume the Pipe's buffer is full if all but one cell is used.
-		if(read_p == ( write_p + 1 ) % (buff_size))
+	if(read_p == ( write_p + 1 ) % BUFF_SIZE)
 	{			
 		// Will sleep until buffer isn't empty.
 		kernel_wait(& pipe->hasSpace, SCHED_PIPE);
+		write_p = pipe->write_p;
+		read_p = pipe->read_p;
+	}
+
+	// Gets available space on Pipe's buffer.
+	//@TODO FIX
+
+	int available_space = (write_p - read_p + BUFF_SIZE) % BUFF_SIZE;
+
+	if(available_space == 0)
+	{
+		available_space = BUFF_SIZE - 1;
+	}
+
+	if((available_space + write_p) % BUFF_SIZE == read_p)
+	{
+		available_space--;
+	}
+
+	for (int i = 0; i < available_space; ++i)
+	{
+		pipe->buffer[write_p] = buf[i];
+		bytes_writen++;
+
+		// Increments write pointer by 1, cycling through the array;
+		write_p = (write_p + 1) %(BUFF_SIZE);
+
+		if (bytes_writen == size)
+		{
+			break;
+		}
+	}
+
+	if (bytes_writen > 0)
+	{
+		// Update write pointer;
+		pipe->write_p = write_p;
+		// Wake up any sleeping readers.
+		Cond_Broadcast(&pipe->hasData);
+		return bytes_writen;
 	}
 	else
-	{	
-		// Gets available space on Pipe's buffer.
-		int available_space = write_p - read_p;
-		if(available_space == 0)
-		{
-			available_space = buff_size - 1;
-		}		
-
-		if (available_space >= size)
-		{
-			for (int i = 0; i < size; ++i)
-			{
-				pipe->buffer[write_p] = buf[i];
-
-				// Increments write pointer by 1, cycling through the array;
-				write_p = (write_p + 1) %(buff_size);
-			}
-			
-			// Wake up any sleeping readers.
-			Cond_Broadcast(&pipe->hasData);
-			return size;
-		}
-		else;
-		{
-			for (int i = 0; i < available_space; ++i)
-			{
-				pipe->buffer[write_p] = buf[i];
-
-				// Increments write pointer by 1, cycling through the array;
-				write_p = (write_p + 1) %(buff_size);
-			}
-			
-			// Wake up any sleeping readers.
-			Cond_Broadcast(&pipe->hasData);
-			return available_space;
-		}
-		
-
-	}
-
-
-	return -1;
+	{
+		return -1;
+	}	
 }
 
 int writer_close (void* this)
 {
+	PipeCB* pipe = (PipeCB *) this;
+	pipe->writer_closed = 1;
 	return -1;
 }
 
@@ -160,15 +173,11 @@ file_ops writer_ops = {
 int sys_Pipe(pipe_t* pipe)
 {
 	FCB* files [2];
-	Fid_t fids [2];
 
-	if(!FCB_reserve(2, fids, files))
+	if(!FCB_reserve(2, (Fid_t*)pipe, files))
 	{
 		return -1;
 	}
-
-	pipe->read = fids[0];
-	pipe->write = fids[1];
 
 	PipeCB * pcb = (PipeCB *)malloc(sizeof(PipeCB));
 	memset(pcb, 0, sizeof(PipeCB));
